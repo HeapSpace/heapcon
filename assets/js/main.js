38c2ee662aec01db489e68f9c9462b1d76864732
Igor Spasić <igor@jodd.org>
2018-08-28 23:50:46 +0200

    Effect applied

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,36 @@ $('.checkbox-toggle').click(function(){
 	$('.btn4').toggleClass('open');
 });
 
-function applyTopColorEffect() {
-	$("body").addClass("bg-red").removeClass("bg-black");
-
-	$(window).scroll(function() {
-		const height = $(window).scrollTop();
-		console.log(height);
-		if (height > 500) {
-			$("body").removeClass("bg-color-0").addClass("bg-color-1");
-    	}
-	});
+/* Calculates the next color */
+const colors = ["#384af4", "#fd4000"];
+var colorNdx;
+function nextColor() {
+	colorNdx = Math.floor(Math.random() * colors.length);
+	return colors[colorNdx];
 }
 
+function applyRandomBackgroundColor() {
+    var stylesheet = document.styleSheets[1];
+    stylesheet.cssRules[0].style.backgroundColor = nextColor();
+}
+
+let direction = 1;
 function initNonHomePage() {
-	applyTopColorEffect();
+	$(window).scroll(function() {
+		const height = $(window).scrollTop();
+
+		if (direction === 1) {
+			if (height > 500) {
+				$("body").css("backgroundColor", "#231f20");
+				direction = 2;
+	    	}
+	    }
+	    else {
+	    	console.log(height);
+			if (height < 400) {
+				$("body").css("backgroundColor", nextColor());
+				direction = 1;
+	    	}
+	    }
+	});
 }
