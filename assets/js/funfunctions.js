fa0625d5992202b78ca1be31d41b5c764c9794e7
Igor Spasić <igor@jodd.org>
2018-09-02 19:20:30 +0200

    rotate jq

--- assets/js/funfunctions.js
+++ assets/js/funfunctions.js
@@ -1,11 +1,19 @@
 // inputs for all methods:
 // y - top offset (scroll-dependent)
 
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+
 // simple left to right movement
 function fun1(y) {
 	x = (30 * Math.sin(y/80));
 	y = 100 * Math.cos(y/80);
-	return {x: x, y: y};
+	return {x: x, y: y, a: y/2};
 }
 
 
@@ -30,6 +38,7 @@ function makefun() {
 		if (inViewport($e)) {
 			$e.css({'left': left + xy.x});
 			$e.css({'top': top + xy.y});
+			$e.rotate(xy.a);
 		}
 	});
 }
