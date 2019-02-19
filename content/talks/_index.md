cd06fea8df23e9810b24893f92254fcb78a05167
Stefan Fejes <ns.fejes.stefan@gmail.com>
2019-02-19 12:36:09 +0100

    resolves #7

--- content/talks/_index.md
+++
@@ -1,7 +0,0 @@
----
-title: Talks
-outputs:
-- html
-- json
-- csv
----
---
+++ index.js
@@ -0,0 +1,7 @@
+const util = require("./fns/util")
+
+const SPEAKERS_DIR_PATH = "content/speakers"
+const TALKS_DIR_PATH = "content/talks"
+
+util.cleanup(SPEAKERS_DIR_PATH)
+util.cleanup(TALKS_DIR_PATH)
