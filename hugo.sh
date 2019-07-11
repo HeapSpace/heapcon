d4c0f460a4af7b78fe5d65310504d38bb92e7e2a
Igor Spasić <igor@jodd.org>
2019-07-11 18:57:09 +0200

    admin

--- hugo.sh
+++ hugo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#set -e
+set -e
 
 include () {
     [[ -f "$1" ]] && source "$1"
@@ -11,3 +11,5 @@ include ".env"
 #node index.js
 
 hugo --gc --minify "$@"
+
+sed -i "" "s/ADMIN_PASSWORD/${ADMIN_PASSWORD}/g" public/_headers
