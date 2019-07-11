a18a6e7a2db72177ef57101ca4e4f1dacb5878a9
Igor Spasić <igor@jodd.org>
2019-07-11 23:39:57 +0200

    admin

--- hugo.sh
+++ hugo.sh
@@ -16,6 +16,4 @@ echo "Running HUGO"
 
 hugo --gc --minify "$@"
 
-ls ./public/_headers
-
-#sed -i "s/ADMIN_PASSWORD/${ADMIN_PASSWORD}/g" ./public/_headers
+sed -i "s/ADMIN_PASSWORD/${ADMIN_PASSWORD}/g" ./public/_headers
